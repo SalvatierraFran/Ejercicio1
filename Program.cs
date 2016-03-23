f58b1e770e4d7012b831ea25826418102e6891bb
SalvatierraFran <franco14_93@hotmail.com>
2016-03-23 20:58:58 -0300

    Primer commit. Se ingresa 5 numeros y devuelve el máximo.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ejercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            int max = 0;
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write("Ingrese numero: ");
+                num = int.Parse(Console.ReadLine());
+
+                if (num > max)
+                {
+                    max = num;
+                }
+            }
+
+            Console.Write("El maximo es " + max);
+            Console.ReadKey();
+        }
+    }
+}
