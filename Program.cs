047851b6c714a6d1b692a41980484d687dc67be4
SalvatierraFran <franco14_93@hotmail.com>
2016-03-23 21:32:57 -0300

    Segundo commit. Ahora también devuelve el menor número de los cinco ingresados.

--- Program.cs
+++ Program.cs
@@ -11,7 +11,9 @@ namespace Ejercicio1
         {
             int num;
             int max = 0;
-
+            int min = 0;
+            int flag = 0;
+            
             for (int i = 0; i < 5; i++)
             {
                 Console.Write("Ingrese numero: ");
@@ -21,9 +23,21 @@ namespace Ejercicio1
                 {
                     max = num;
                 }
+
+                if (flag == 0 && min == 0)
+                {
+                    min = num;
+                    flag = 1;
+                }
+
+                if (num < min)
+                {
+                    min = num;
+                }
             }
 
             Console.Write("El maximo es " + max);
+            Console.Write("\nEl minimo es " + min);
             Console.ReadKey();
         }
     }
