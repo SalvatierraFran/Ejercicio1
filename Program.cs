d29a774a68c7a9b74136e8e5690dd05fd5bd3bea
SalvatierraFran <franco14_93@hotmail.com>
2016-03-23 22:52:27 -0300

    Tercer commit. Se le agregó la devolución del promedio de los cinco números.

--- Program.cs
+++ Program.cs
@@ -13,6 +13,8 @@ namespace Ejercicio1
             int max = 0;
             int min = 0;
             int flag = 0;
+            int acum = 0;
+            float resultado = 0;
             
             for (int i = 0; i < 5; i++)
             {
@@ -34,10 +36,15 @@ namespace Ejercicio1
                 {
                     min = num;
                 }
+
+                acum = acum + num;
+
+                resultado = (float)acum / 5;
             }
 
             Console.Write("El maximo es " + max);
             Console.Write("\nEl minimo es " + min);
+            Console.Write("\nEl promedio es {0:f2}", resultado);
             Console.ReadKey();
         }
     }
